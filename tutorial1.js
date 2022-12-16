87dfcbaa28e7679c3189c4f02c7e49b6c3da3136
aditya <adityapatelait786@gmail.com>
2022-12-16 14:27:31 +0530

    third

--- tutorial1.js
+++ tutorial1.js
@@ -1,3 +1,18 @@
 console.log ("hello world")  ;
 console.log ("change")
 
+// variables in java script
+
+let a = 10 ;            // block level
+var b = 11 ;            // global level 
+{
+    let a = 15 ;        // block level
+    var b = 12 ;        // global variable .
+    console.log(a) ;    // 
+    console.log(b) ;    // 
+}
+
+console.log( a) ;       // 
+console.log( b) ;       // 
+
+
