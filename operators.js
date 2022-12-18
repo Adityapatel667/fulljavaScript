fba663bfbe6a41d554ac8831dea2f153956d335b
aditya <adityapatelait786@gmail.com>
2022-12-18 15:50:33 +0530

    operator

--- operators.js
+++ operators.js
@@ -14,4 +14,42 @@ console.log ("a-- = ",a-- )
 
 
 // assignment operators .
+let c = 10 ;
+let d = 5;
+ console.log("equal to " , c=d) ;
+ console.log("addition " , c+=d) ;
+ console.log("substraction " , c-=d) ;
+ console.log("divide " , c/=d) ;
+ console.log("modulas " , c%=d) ;
+ console.log("greater than or equal to" , c>=d) ;
+ console.log("less than or equal to" , c<=d) ;
+
+//   comparison operator 
+
+// console.Log("equal to " , c==d)
+// console.Log("equal to and type" , c===d)
+// console.Log("not equal to and type" , c!==d)
+// console.log("greater than " , c>d) ;
+// console.log("less than " , c<d) ;
+
+/* multiline comment */ 
+
+// logical operator 
+
+// console.log("and operator" ,c&&d)
+// console.log("or operator" ,c||d)
+// console.log("not operator" ,c!=d)
+
+
+// logical operators
+
+
+
+
+
+
+
+
+
+
 
