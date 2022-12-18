ed1354bbdd45c51f1947556340ebfa8c830b0fe8
aditya <adityapatelait786@gmail.com>
2022-12-18 11:16:07 +0530

    operator

---
+++ operators.js
@@ -0,0 +1,17 @@
+
+
+let a = 4 ;
+let b = 45 ;
+// arithmatic operators
+console.log ("a+b = ",a+b , "addition" )
+console.log ("a-b = ",a-b , '' )
+console.log ("a/b = ",a/b )
+console.log ("a%b = ",a%b )
+console.log ("++a = ",++a )
+console.log ("a++ = ",a++ )
+console.log ("--a = ",--a )
+console.log ("a-- = ",a-- )
+
+
+// assignment operators .
+
