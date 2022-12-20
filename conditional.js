808bed700a22da1473ff3615591179a12e85bd64
aditya <adityapatelait786@gmail.com>
2022-12-20 10:55:57 +0530

    practice

---
+++ conditional.js
@@ -0,0 +1,24 @@
+
+// let a=  prompt ("what is your age") ;
+
+// if (a<0){
+//   console.log  (" enter a valid age")
+// }
+
+// else if (a<10){
+//     console.log("you are a kid")
+// }
+// else {
+//     console.log("you can drive ")
+// }
+
+// let a = prompt ("write you switch condition age ")
+// let a = number.parseInt(a)
+// switch (age ){
+//     case 12 : 
+//     console.log ("your age is 12")
+//     case 14 : 
+//     console.log("your age is 14 ")
+//     default :
+//     console.log("your age is not special ")
+// }
---
+++ practice.js
@@ -0,0 +1,18 @@
+
+
+
+var a = 10 ;
+var b = 15 ;
+
+function add( ){
+    let add = a+b ;
+    console.log("addition" ,add)
+    // sub()
+}
+
+function sub(){
+    let sub = a-b
+    console.log(sub)
+}
+
+add (sub() ) ;
